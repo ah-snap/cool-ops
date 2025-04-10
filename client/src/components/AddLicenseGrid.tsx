02aee8c26215c75e362ac0b4ddc57be6e425ac93
Allan Harper <Allan.Harper@snapone.com>
2025-04-10 12:36:20 -0600

    Small cleanup

--- client/src/components/AddLicenseGrid.tsx
+++ client/src/components/AddLicenseGrid.tsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { LicenseData, LicenseRequestBody } from "../types.t";
+import KeyValueRow from "./KeyValueRow";
 
 
-async function licenseProcessCall(requestBody) {
-    const request = { 
-        method: "POST", 
+async function licenseProcessCall(requestBody: LicenseRequestBody) {
+    const request = {
+        method: "POST",
         body: JSON.stringify(requestBody),
-        headers: { "X-BACKWARDS-USER": '{"userId": 2906087, "accountId": 2630480, "internal": true}',
-        "Content-Type": "application/json"
-         } };
-    const response = await fetch(`http://localhost:8061/v1/customers/C561682/licenses`, request );
+        headers: {
+            "X-BACKWARDS-USER": '{"userId": 2906087, "accountId": 2630480, "internal": true}',
+            "Content-Type": "application/json"
+        }
+    };
+    const response = await fetch(`http://localhost:8061/v1/customers/C561682/licenses`, request);
     const data = await response.json();
     console.log("Request", request, "Response", response, "Data", data);
     console.log(data);
 
     return data;
-
 }
 
-function populateLicenseAsAddAssist(licenseRow : LicenseData) : LicenseRequestBody {
+function populateLicenseAsAddAssist(licenseRow: LicenseData): LicenseRequestBody {
     //Control4 Assist | Control4 Assist Premium
     return {
         productName: "Control4 Assist Premium",
         transactionId: `ASSIST_FREE-${crypto.randomUUID()}-Assist`,
         skus: ["C4-ASSIST-PREMIUM"],
-        createdTime: licenseRow.created_time.replace("T", " ").replace(".", ",").replace("Z",""),
+        createdTime: licenseRow.created_time.replace("T", " ").replace(".", ",").replace("Z", ""),
         accountId: licenseRow.account_id,
         isRecurring: false,
         vendor: "D365",
@@ -38,8 +40,8 @@ function populateLicenseAsAddAssist(licenseRow : LicenseData) : LicenseRequestBo
 }
 
 
-export default function AddLicenseGrid({ licenseRow } : { licenseRow: LicenseData }) {
-    const [ requestBody, setRequestBody ] = useState<LicenseRequestBody | null>(null);
+export default function AddLicenseGrid({ licenseRow }: { licenseRow: LicenseData }) {
+    const [requestBody, setRequestBody] = useState<LicenseRequestBody | null>(null);
 
     useEffect(() => {
         if (licenseRow) {
@@ -56,34 +58,14 @@ export default function AddLicenseGrid({ licenseRow } : { licenseRow: LicenseDat
             <h3>Adding this license <button onClick={() => licenseProcessCall(requestBody)}>Submit</button></h3>
             <table>
                 <tbody>
-                    <tr>
-                        <td>SKU</td>
-                        <td>{requestBody.skus && requestBody.skus.join(", ")}</td>
-                    </tr>
-                    <tr>
-                        <td>Transaction Id</td>
-                        <td>{requestBody.transactionId}</td>    
-                    </tr>
-                    <tr>
-                        <td>Account Id</td>
-                        <td>{requestBody.accountId}</td>
-                    </tr>
-                    <tr>
-                        <td>Created Time</td>
-                        <td>{requestBody.createdTime}</td>
-                    </tr>
-                    <tr>
-                        <td>Is Recurring</td>
-                        <td>{requestBody.isRecurring ? "true" : "false"}</td>
-                    </tr>
-                    <tr>
-                        <td>Vendor</td>
-                        <td>{requestBody.vendor}</td>
-                    </tr>
-                    <tr>
-                        <td>D365 Customer Id</td>
-                        <td>{requestBody.d365CustomerId}</td>
-                    </tr>
+                    <KeyValueRow label="SKU" value={requestBody.skus && requestBody.skus.join(", ")} />
+                    <KeyValueRow label="Product Name" value={requestBody.productName} />
+                    <KeyValueRow label="Transaction ID" value={requestBody.transactionId} />
+                    <KeyValueRow label="Account ID" value={requestBody.accountId} />
+                    <KeyValueRow label="Created Time" value={requestBody.createdTime} />
+                    <KeyValueRow label="Is Recurring" value={requestBody.isRecurring ? "true" : "false"} />
+                    <KeyValueRow label="Vendor" value={requestBody.vendor} />
+                    <KeyValueRow label="D365 Customer Id" value={requestBody.d365CustomerId} />
                 </tbody>
             </table>
         </div>
